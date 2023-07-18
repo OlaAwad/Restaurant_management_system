ef639143f61d2ef58f911e9e3650202a4a88dcc0
OlaAwad <ola.khireldin@gmail.com>
2023-07-18 17:01:00 +0300

    init payment

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -18,6 +18,17 @@ export class PaymentComponent implements OnInit {
   orderTypes: string[] = ['Dine in', 'To Go', 'Delivery']
   selectedType: string = ''
   selectedMethodIndex = 0
+  paymentDetails={
+    CustomerName:'',
+    CustomerMobile: null,
+    paidAmount:{
+      debit: 0,
+      cash: 0
+    }
+  }
+  wrongPaidAmount: boolean = false
+
+  // cashAmount: number = 0
 
   constructor(
     private cartService: CartService,
@@ -196,12 +207,16 @@ export class PaymentComponent implements OnInit {
     if (this.isFormValid()) {
       this.cartService.cartItems$.pipe(take(1)).subscribe((cartItems: Product[]) => {
         
-        let customerMobileString = (<HTMLInputElement>(
-          document.getElementById('CustomerMobile')
-        ))?.value
-        let tableNoString = (<HTMLInputElement>(
-          document.getElementById('TableNo')
-        ))?.value
+        let customerName = (<HTMLInputElement>document.getElementById('CustomerName'))?.value
+        let customerMobile = (<HTMLInputElement>document.getElementById('CustomerMobile'))?.value
+        let deliveryAddress = (<HTMLInputElement>document.getElementById('address'))?.value
+        let dineInTableNo = (<HTMLInputElement>document.getElementById('TableNo'))?.value
+        // let customerMobileString = (<HTMLInputElement>(
+        //   document.getElementById('CustomerMobile')
+        // ))?.value
+        // let tableNoString = (<HTMLInputElement>(
+        //   document.getElementById('TableNo')
+        // ))?.value
   
         // let productIds = cartItems.map((item) => item.id)
   
@@ -210,72 +225,139 @@ export class PaymentComponent implements OnInit {
           ProductName: cartItems.map((item) => item.ProductName).join(', '),
           ProductQuantity: cartItems.map((item) => item.ProductQuantity).join(', '),
           ProductNotes: cartItems.map((item) => item.ProductNotes).join(', '),
-          ProductPrice: cartItems.map((item) => item.ProductPrice).join(', '),
+          ProductPrice: cartItems.map((item)=> item.ProductPrice).join(', '),
           TotalPrice: 0,
           OrderDate: new Date(),
           OrderStatus: 'Pending',
-          CustomerName: (<HTMLInputElement>(
-            document.getElementById('CustomerName')
-          ))?.value,
-          CustomerMobile: customerMobileString
-            ? parseInt(customerMobileString)
-            : undefined,
-          TableNo: tableNoString ? parseInt(tableNoString) : undefined,
-          Address: (<HTMLInputElement>document.getElementById('address'))?.value,
+          CustomerName: customerName,
+          CustomerMobile: customerMobile ? parseInt(customerMobile): undefined,
+          TableNo: dineInTableNo ? parseInt(dineInTableNo): undefined,
+          Address: this.selectedType === 'Delivery' ? deliveryAddress: undefined,
+          DeliveryFees: this.selectedType === 'Delivery' ? 40 :  undefined,
+          ServiceFees: this.selectedType === 'Dine in' ? 0.14: undefined
+        }
+        // let order: Order = {
+        //   OrderType: this.selectedType,
+        //   ProductName: cartItems.map((item) => item.ProductName).join(', '),
+        //   ProductQuantity: cartItems.map((item) => item.ProductQuantity).join(', '),
+        //   ProductNotes: cartItems.map((item) => item.ProductNotes).join(', '),
+        //   ProductPrice: cartItems.map((item) => item.ProductPrice).join(', '),
+        //   TotalPrice: 0,
+        //   OrderDate: new Date(),
+        //   OrderStatus: 'Pending',
+        //   CustomerName: (<HTMLInputElement>(
+        //     document.getElementById('CustomerName')
+        //   ))?.value,
+        //   CustomerMobile: customerMobileString
+        //     ? parseInt(customerMobileString)
+        //     : undefined,
+        //   TableNo: tableNoString ? parseInt(tableNoString) : undefined,
+        //   Address: (<HTMLInputElement>document.getElementById('address'))?.value,
+        // }
+
+        let totalPrice = 0
+        cartItems.forEach((item) => {
+          totalPrice += item.ProductPrice * (item.ProductQuantity ?? 0)
+        })
+  
+        // let totalPrice = cartItems.reduce(
+        //   (total: any, item: Product) =>
+        //     total + item.ProductPrice * item.ProductQuantity!,
+        //   0,
+        // )
+
+        if(this.selectedType === 'Delivery'){
+          totalPrice += order.DeliveryFees ?? 0
+        }else if(this.selectedType === 'Dine in'){
+          totalPrice += totalPrice * (order.ServiceFees ?? 0)
+        }
+
+        order.TotalPrice = totalPrice
+        console.log('totalPrice: ', order.TotalPrice)
+
+        let cashPaid = parseFloat((<HTMLInputElement>document.getElementById('CashPaidAmount'))?.value)
+        console.log('cashPaid: ', cashPaid)
+        let debitPaid = parseFloat((<HTMLInputElement>document.getElementById('DebitPaidAmount'))?.value)
+        console.log('debitPaid: ', debitPaid)
+
+        console.log('+: ', cashPaid + debitPaid)
+        if(cashPaid + debitPaid !== totalPrice){
+          this.wrongPaidAmount = true
+          console.log('not equal')
+          return;
+        }else{
+          this.wrongPaidAmount = false
         }
   
-        let totalPrice = cartItems.reduce(
-          (total: any, item: Product) =>
-            total + item.ProductPrice * item.ProductQuantity!,
-          0,
-        )
+        // if (this.selectedType === 'Delivery') {
+        //   order.DeliveryFees = 40
+        //   totalPrice += order.DeliveryFees
+        // } else if (this.selectedType === 'Dine in') {
+        //   order.ServiceFees = 0.14
+        //   totalPrice = totalPrice + totalPrice * order.ServiceFees
+        // }
   
-        if (this.selectedType === 'Delivery') {
-          order.DeliveryFees = 40
-          totalPrice += order.DeliveryFees
-        } else if (this.selectedType === 'Dine in') {
-          order.ServiceFees = 0.14
-          totalPrice = totalPrice + totalPrice * order.ServiceFees
-        }
-  
-        order.TotalPrice = totalPrice.toFixed(2)
-        order.OrderStatus = 'Pending'
-  
-        if (this.selectedMethodIndex === 0) {
-          let cardNumber = (<HTMLInputElement>(
-            document.getElementById('CardNumber')
-          )).value
-  
-          this.orderService.saveOrder(order).subscribe(() => {
-            console.log('order: ', order)
-            // Update the available quantity of products
-            cartItems.forEach((item) => {
-              item.ProductAvailableQuantity -= item.ProductQuantity!
-              console.log('item: ', item)
-              console.log('AQ', item.ProductAvailableQuantity)
-              this.productService.updateProductII(item.ProductId!, item).subscribe()
-            })
-            // Empty the cart and close the payment dialog
-            this.cartService.clearCart()
-            this.cartService.sendPaymentFlag(false)
-            this.sendCartFlag(false)
+        // order.TotalPrice = totalPrice.toFixed(2)
+        // order.OrderStatus = 'Pending'
+
+        //Submit order:
+
+        this.orderService.saveOrder(order).subscribe((savedOrder) => {
+          console.log('order: ', savedOrder)
+
+          //update available quantity:
+          cartItems.forEach((item) => {
+            item.ProductAvailableQuantity -= item.ProductQuantity ?? 0
+            this.productService.updateProduct(item).subscribe()
           })
-        } else if (this.selectedMethodIndex === 1) {
-          this.orderService.saveOrder(order).subscribe(() => {
-            console.log('order: ', order)
-            // Update the available quantity of products
-            cartItems.forEach((item) => {
-              item.ProductAvailableQuantity -= item.ProductQuantity!
-              this.productService.updateProductII(item.ProductId!, item).subscribe()
-            })
-            // Empty the cart and close the payment dialog
-            this.cartService.clearCart()
-            this.cartService.sendPaymentFlag(false)
-            this.sendCartFlag(false)
-          })
-        }
+
+          //Empty cart and close dialog:
+          this.cartService.clearCart()
+          this.cartService.sendPaymentFlag(false)
+          this.sendCartFlag(false)
+        })
+
+        // if (this.selectedMethodIndex === 0) {
+        //   let cardNumber = (<HTMLInputElement>(
+        //     document.getElementById('CardNumber')
+        //   )).value
+  
+        //   this.orderService.saveOrder(order).subscribe(() => {
+        //     console.log('order: ', order)
+        //     // Update the available quantity of products
+        //     cartItems.forEach((item) => {
+        //       item.ProductAvailableQuantity -= item.ProductQuantity!
+        //       console.log('item: ', item)
+        //       console.log('AQ', item.ProductAvailableQuantity)
+        //       this.productService.updateProductII(item.ProductId!, item).subscribe()
+        //     })
+        //     // Empty the cart and close the payment dialog
+        //     this.cartService.clearCart()
+        //     this.cartService.sendPaymentFlag(false)
+        //     this.sendCartFlag(false)
+        //   })
+        // } else if (this.selectedMethodIndex === 1) {
+        //   this.orderService.saveOrder(order).subscribe(() => {
+        //     console.log('order: ', order)
+        //     // Update the available quantity of products
+        //     cartItems.forEach((item) => {
+        //       item.ProductAvailableQuantity -= item.ProductQuantity!
+        //       this.productService.updateProductII(item.ProductId!, item).subscribe()
+        //     })
+        //     // Empty the cart and close the payment dialog
+        //     this.cartService.clearCart()
+        //     this.cartService.sendPaymentFlag(false)
+        //     this.sendCartFlag(false)
+        //   })
+        // }
       })
     }
+  }
+
+  calculateCashAmount(){
+    const cashPaid = parseFloat((<HTMLInputElement>document.getElementById('CashPaidAmount'))?.value)
+    const debitPaid = parseFloat((<HTMLInputElement>document.getElementById('DebitPaidAmount'))?.value)
+    // this.paymentDetails.paidAmount.cash = totalPrice - debitPaid
   }
 
   sendCartFlag(flag: boolean) {
